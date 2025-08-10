3252aa768b7cf12fcdde3f0f155b083eca6fba42
OMDANY1 <emadadelgd@gmail.com>
2025-08-10 12:15:54 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,2 +1,15 @@
 import type { Config } from 'tailwindcss'
-const config: Config={content:['./app/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}'],theme:{extend:{container:{center:true,padding:'1rem'}}},plugins:[require('@tailwindcss/typography')],};export default config
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const typography = require('@tailwindcss/typography')
+
+const config: Config = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      container: { center: true, padding: '1rem' },
+    },
+  },
+  plugins: [typography],
+}
+export default config

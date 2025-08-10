d45da77b1a6d1695085ff772e26b7d7ede7b75fd
OMDANY1 <emadadelgd@gmail.com>
2025-08-10 13:46:28 +0300

    Create page.tsx

---
+++ app/studio/[[...index]]/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { NextStudio } from 'next-sanity/studio'
+import config from '@/sanity.config'
+
+export default function StudioPage() {
+  return <div className="min-h-screen"><NextStudio config={config} /></div>
+}

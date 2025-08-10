091e0dda3f44b399d71b7a949dd6119007f36457
OMDANY1 <emadadelgd@gmail.com>
2025-08-10 14:24:48 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**.cdn.sanity.io' }],
   },
-}
-module.exports = nextConfig
+};
